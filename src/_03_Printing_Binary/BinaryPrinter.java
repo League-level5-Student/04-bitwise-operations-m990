69577977193a35ed044c354783cfff98ab11c9ba
Sofisa <league@iMac-2.attlocal.net>
2018-10-29 19:57:11 -0700

    base64

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -6,18 +6,96 @@ public class BinaryPrinter {
 	//Create a main method to test your methods.
 	
 	public void printByteBinary(byte b) {
-		
+		System.out.println(b);
+		int n = b;
+		int[] binaryNum = new int[1000];
+    int i = 0;
+    while (n > 0) 
+    {
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+    String[] output = new String[1000];
+    for(int m = 0; m < binaryNum.length; m++) {
+    		output[m] = binaryNum[m]+"";
+    }
+    String p = "";
+    for (int j = i - 1; j >= 0; j--) {
+    		p += output[j];
+    }
+    System.out.println(p);
 	}
 	
 	public void printShortBinary(short s) {
-		
+		int n = s;
+		int[] binaryNum = new int[1000];
+    int i = 0;
+    while (n > 0) 
+    {
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+    String[] output = new String[1000];
+    for(int m = 0; m < binaryNum.length; m++) {
+    		output[m] = binaryNum[m]+"";
+    }
+    String p = "";
+    for (int j = i - 1; j >= 0; j--) {
+    		p += output[j];
+    }
+    System.out.println(p);
 	}
 	
 	public void printIntBinary(int i) {
-		
+		int n = i;
+		int[] binaryNum = new int[1000];
+    while (n > 0) 
+    {
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+    String[] output = new String[1000];
+    for(int m = 0; m < binaryNum.length; m++) {
+    		output[m] = binaryNum[m]+"";
+    }
+    String p = "";
+    for (int j = i - 1; j >= 0; j--) {
+    		p += output[j];
+    }
+    System.out.println(p);
 	}
 	
 	public void printLongBinary(long l) {
-		
+		long n = l;
+		int[] binaryNum = new int[1000];
+    int i = 0;
+    while (n > 0) 
+    {
+        binaryNum[i] = (int) (n % 2);
+        n = n / 2;
+        i++;
+    }
+    String[] output = new String[1000];
+    for(int m = 0; m < binaryNum.length; m++) {
+    		output[m] = binaryNum[m]+"";
+    }
+    String p = "";
+    for (int j = i - 1; j >= 0; j--) {
+    		p += output[j];
+    }
+	}
+	public static void main(String[] args) {
+		BinaryPrinter bp = new BinaryPrinter();
+		byte a = 68;
+		bp.printByteBinary(a);
+		short b = 5;
+		bp.printShortBinary(b);
+		int c = 123;
+		bp.printIntBinary(c);
+		long d = 123453653;
+		bp.printLongBinary(d);
 	}
 }
